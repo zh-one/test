f4d8e75c20ddcaddc116d817d89be5fc259552e1
lianzhang <lianzhang2@iflytek.com>
2020-02-16 16:38:04 +0800

    for test branch myself

--- One.java
+++ One.java
@@ -3,6 +3,6 @@
     public class One{
    
         public static void main(String[] args){
-	    System.out.println("git hub and the test") ;
+	    System.out.println("git hub and myself") ;
 	}
     }
