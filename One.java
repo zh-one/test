d1cfcde88c16e3be0751696b04d1008f41b7083f
lianzhang <lianzhang2@iflytek.com>
2020-02-17 12:18:35 +0800

    ordertest

--- One.java
+++ One.java
@@ -3,6 +3,6 @@
     public class One{
    
         public static void main(String[] args){
-	    System.out.println("git hub and master and the shell order") ;
+	    System.out.println("git hub and master and the shell order ====") ;
 	}
     }
