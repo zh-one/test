398cade799320c6b662aa66d5d8fceee62aa9d04
lianzhang <lianzhang2@iflytek.com>
2020-02-19 12:44:56 +0800

    it is test

--- Two.java
+++ Two.java
@@ -2,7 +2,8 @@
 
     public class Two{
        public static void main(String[] args){
-           System.out.println("your input is " + args[0]) ;
+           System.out.println("it is test two") ;
+	   System.out.println("your input is " + args[0]) ;
        }
     }
 
