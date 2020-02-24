9656a638c361f93d3cbf2037186fa87d08fb73ad
lianzhang <lianzhang2@iflytek.com>
2020-02-24 12:44:29 +0800

    it is test

--- One.java
+++ One.java
@@ -3,16 +3,9 @@
     public class One{
    
         public static void main(String[] args){
-<<<<<<< HEAD
 	    System.out.println("git hub and one") ;
 	    for (int i = 0 ; i < 10 ; i ++){
-=======
-	    System.out.println("git hub and dev") ;
-	    for (int i = 0 ; i < 15 ; i ++){
->>>>>>> master
-               System.out.println("the i is " + i) ;
-	       System.out.println("it is test") ;
-	
+	        System.out.println("the i is " + i) ;
 	    }
 	}
     }
