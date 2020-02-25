574137239230ff9dfba36ca3feaa7e2b78d09e56
lianzhang <lianzhang2@iflytek.com>
2020-02-25 11:17:09 +0800

    it is one

--- One.java
+++ One.java
@@ -3,7 +3,7 @@
     public class One{
    
         public static void main(String[] args){
-	    System.out.println("git hub and One") ;
+	    System.out.println("git hub and One local one") ;
 	    for (int i = 10 ; i < 15 ; i ++){
                System.out.println("the i is " + i) ;
 	    }
