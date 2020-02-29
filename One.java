97fadf17b1a43de8aad8c6d0f36f2f81d18af952
lianzhang <lianzhang2@iflytek.com>
2020-02-29 11:06:57 +0800

    it is test

--- One.java
+++ One.java
@@ -5,7 +5,7 @@
         public static void main(String[] args){
 	    System.out.println("git hub and dev") ;
 	    for (int i = 5 ; i < 10 ; i ++){
-               System.out.println("the i is " + i) 
+               System.out.println("the i is " + i) ; 
 	    }
 	}
     }
