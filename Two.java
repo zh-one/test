bca505a429db890d80f12f859e5f87c5988a35e6
lianzhang2 <lianzhang2@iflytek.com>
2020-03-13 18:06:17 +0800

    test for three

--- Two.java
+++ Two.java
@@ -2,7 +2,7 @@
 
     public class Two{
        public static void main(String[] args){
-           System.out.println("it is test two") ;
+           System.out.println("it is test two and three") ;
 	   System.out.println("your input is " + args[0]) ;
        }
     }
