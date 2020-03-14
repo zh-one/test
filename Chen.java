073dcb9b04052a0eade6295b2d420f0899d363ac
lianzhang2 <lianzhang2@iflytek.com>
2020-03-13 23:31:18 -0400

    it is chen

--- Chen.java
+++ Chen.java
@@ -3,6 +3,7 @@
 
        public static void main(String[] args){
           for(int i = 1 ; i < 12 ; i ++ ){
+             System.out.println("it is chen") ;
 	     for(int j = 1 ; j <= i ; j ++ ){
                   System.out.print( j + " * " + i + " = " + (i * j) + " ") ;
 	     }
