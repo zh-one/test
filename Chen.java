0e92e257f1955ec4586fbdfd6c75736404aecfb4
lianzhang2 <lianzhang2@iflytek.com>
2020-03-13 23:33:10 -0400

    it is chen

--- Chen.java
+++ Chen.java
@@ -3,6 +3,7 @@
 
        public static void main(String[] args){
           for(int i = 1 ; i < 12 ; i ++ ){
+             System.out.println("what the master");
 	     for(int j = 1 ; j <= i ; j ++ ){
                   System.out.print( j + " * " + i + " = " + (i * j) + " ") ;
 	     }
